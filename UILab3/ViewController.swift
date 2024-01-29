d1c2f79612d317b8d22637bdd497be0856be1cf4
Athira Mangalath Muraleedharan <Amangalathmural5484@conestogac.on.ca>
2024-01-28 22:56:18 -0500

    Initial Commit

---
+++ UILab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILab3
+//
+//  Created by user235514 on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
