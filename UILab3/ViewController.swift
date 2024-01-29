0a39ef71b524ce62c1214c589fbfcc93f67ff0fb
Athira Mangalath Muraleedharan <Amangalathmural5484@conestogac.on.ca>
2024-01-29 14:13:48 -0500

    UI Lab 3 initial commit

--- UILab3/ViewController.swift
+++ UILab3/ViewController.swift
@@ -1,19 +1,60 @@
-//
-//  ViewController.swift
-//  UILab3
-//
-//  Created by user235514 on 1/28/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultTextView: UITextView!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var country: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.isHidden = true
     }
-
-
+    
+    @IBAction func addButton(_ sender: Any) {
+        viewDetails()
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        if areAllFieldsFilled() {
+            messageLabel.text = "Successfully submitted!"
+            viewDetails()
+        } else {
+            messageLabel.text = "Complete the missing Info!"
+        }
+        messageLabel.isHidden = false
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        firstName.text = ""
+        lastName.text = ""
+        country.text = ""
+        age.text = ""
+        messageLabel.isHidden = true
+        resultTextView.text = ""
+    }
+    
+    private func areAllFieldsFilled() -> Bool {
+        return !(firstName.text?.isEmpty ?? true) &&
+            !(lastName.text?.isEmpty ?? true) &&
+            !(country.text?.isEmpty ?? true) &&
+            !(age.text?.isEmpty ?? true)
+    }
+    
+    private func viewDetails(){
+        let firstNameText = firstName.text ?? ""
+        let lastNameText = lastName.text ?? ""
+        let countryText = country.text ?? ""
+        let ageText = age.text ?? ""
+        
+        resultTextView.text = """
+            Full Name: \(firstNameText) \(lastNameText)
+            Country: \(countryText)
+            Age: \(ageText)
+            """
+    }
 }
 
